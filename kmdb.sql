d580b9f272bbc94422b9c600daca9b99972d39fb
ligeoff1 <geoff.li@kellogg.northwestern.edu>
2022-01-23 15:42:52 +0000

    Insert top_cast

--- kmdb.sql
+++ kmdb.sql
@@ -100,6 +100,31 @@ VALUES
   ("The Dark Knight Rises", "2012", "PG-13", "Christopher Nolan")
 ;
 
+INSERT INTO top_cast (
+  title,    
+  actor,
+  role
+)
+
+VALUES
+  ("Batman Begins", "Christian Bale","Bruce Wayne"),
+  ("Batman Begins", "Michael Caine","Alfred"),
+  ("Batman Begins", "Liam Neeson","Ra's Al Ghul"),
+  ("Batman Begins", "Katie Holmes","Rachel Dawes"),
+  ("Batman Begins", "Gary Oldman","Comissioner Gordon"),
+  ("The Dark Knight", "Christian Bale","Bruce Wayne"),
+  ("The Dark Knight", "Heath Ledger","Joker"),
+  ("The Dark Knight", "Aaron Eckhart","Harvey Dent"),
+  ("The Dark Knight", "Michael Caine","Alfred"),
+  ("The Dark Knight", "Maggie Gyllenhaal","Rachel Dawes"),
+  ("The Dark Knight Rises", "Christian Bale","Bruce Wayne"),
+  ("The Dark Knight Rises", "Gary Oldman","Comissioner Gordon"),
+  ("The Dark Knight Rises", "Tom Hardy","Bane"),
+  ("The Dark Knight Rises", "Joseph Gordon-Levitt","John Blake"),
+  ("The Dark Knight Rises", "Anne Hathaway","Selina Kyle")
+;
+
+
 -- Prints a header for the movies output
 .print "Movies"
 .print "======"
